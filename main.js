2d88fc181eb1b0db5f3f3e8926b95c5a163b2af4
Tony-Thawatchai Saengthamchai <93370758+Tony-Thawatchai@users.noreply.github.com>
2023-06-05 15:48:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// search button 
+searchBtn.addEventListener(`click`, ()=>{
+    searchBar.classList.toggle("top-bar-active")
+})
+
+menuBtn.addEventListener(`click`, ()=>{
+    menuBar.classList.toggle("top-bar-active")
+})
